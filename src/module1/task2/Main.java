b5c0a180b36e8f0afafcac645dcc77ec847820d8
Onoxider <onoxider@gmail.com>
2017-06-08 18:26:20 +0300

    Reassuring that commit works correctly

--- src/module1/task2/Main.java
+++ src/module1/task2/Main.java
@@ -9,15 +9,22 @@ import java.lang.Math;
 public class Main {
     public static void main(String[] args)  {
 
+        Double e=0.0;
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            System.out.println("Введите значение эпсилон");
-            Double e = Double.parseDouble(br.readLine());
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Введите значение эпсилон: ");
+            e = Double.parseDouble(br.readLine());
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
 
+        int index = 1;
+        double a = 0.0;
 
-
+        do {
+            a = 1 / (Math.pow(index + 1, 2));
+            index++;
+            System.out.println(a);
+        } while (a >= e);
     }
 }
 
