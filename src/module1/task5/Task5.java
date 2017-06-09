20701c2eb7328eef7a0423d509cf34c634ccf426
Onoxider <onoxider@gmail.com>
2017-06-09 19:02:22 +0300

    Reassuring that commit works correctly

---
+++ src/module1/task5/Task5.java
@@ -0,0 +1,46 @@
+package module1.task5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by yokuras on 09.06.2017.
+ */
+public class Task5 {
+    public static void main(String[] args) {
+
+        int a, b, h;
+        a = b = h = 0;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Введите размерность матрицы:");
+            a = Integer.parseInt(br.readLine());
+            //   b = Double.parseDouble(br.readLine());
+            //  h = Double.parseDouble(br.readLine());
+        } catch (IOException ex) {
+        }
+
+        int[][] matrix = new int[a][a];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                if (i == j) {
+                    matrix[i][j] = 1;
+                }else if(i == matrix.length - j - 1) {
+                    matrix[i][j] = 1;
+                }
+                else matrix[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                System.out.print(matrix[i][j]+ "  ");
+            }
+            System.out.println();
+        }
+
+    }
+}
